f39f3a59fa9049ec23eb54bc5bea73756ae47173
Rafael Bergo <rafaelbergo503@gmail.com>
2023-12-16 23:27:38 -0300

    adicionado html e templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'recipes/home.html')
 
 def contato(request):
     return HttpResponse('Contato')
