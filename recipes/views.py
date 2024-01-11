8b551b51d3f4d324770d6a713ff2011f91fb26e3
Rafael Bergo <rafaelbergo503@gmail.com>
2024-01-11 19:23:13 -0300

    modificado template base e adicionado herança

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={'name': 'NOME'})
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={'name': 'NOME'})
