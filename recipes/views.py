03cc4eecdeca6a429e9698eb1b5e5d6de54681f6
Rafael Bergo <rafaelbergo503@gmail.com>
2024-01-16 23:38:06 -0300

    modificado template recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={'name': 'NOME'})
+    return render(request, 'recipes/pages/home.html', context={'recipes': [make_recipe() for _ in range(10)]})
+
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', context={'name': 'NOME'})
+    return render(request, 'recipes/pages/recipe-view.html', context={'recipe': make_recipe(), 'is_detail_page': True})
